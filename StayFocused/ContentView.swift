20ec0d8ce08494bc4b007b0c3ff99bce4ae92145
MaksGorobets <gorobets542@gmail.com>
2025-04-28 12:25:52 +0300

    Initial Commit

---
+++ StayFocused/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StayFocused
+//
+//  Created by Maks Winters on 28.04.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StayFocused/StayFocusedApp.swift
@@ -0,0 +1,17 @@
+//
+//  StayFocusedApp.swift
+//  StayFocused
+//
+//  Created by Maks Winters on 28.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct StayFocusedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
